b94f8a888c42d259fed749b37b699dbad7f262bd
Soyas Limbu <90306699+soyas07@users.noreply.github.com>
2021-12-07 23:35:08 +1100

    updated mobile version
    Fixed: Margins and overflow components in smaller screen

--- src/components/Abilities.js
+++ src/components/Abilities.js
@@ -5,11 +5,13 @@ function Abilities( {data, breakPoint} ) {
 
     const listSlider = data.map(item => (
         <div className="slider-image">
-            <svg id="Group_66" data-name="Group 66" xmlns="http://www.w3.org/2000/svg" width="72.651" height="78.76" viewBox="0 0 72.651 78.76">
-                <path id="Path_56" data-name="Path 56" d="M393.967,486.82,357.638,509.8,321.317,486.82l36.321-22.844Zm0,0" transform="translate(-321.317 -463.976)" fill="#ffe01a"/>
-                <path id="Path_57" data-name="Path 57" d="M357.638,493.844,335.1,479.953l-13.783,8.436,36.321,22.384,36.329-22.384-13.781-8.436Zm0,0" transform="translate(-321.316 -448.543)" fill="#ff6f00"/>
-                <path id="Path_58" data-name="Path 58" d="M357.639,502.646,335.1,488.753l-13.783,8.438,36.322,22.384,36.329-22.384-13.781-8.438Zm0,0" transform="translate(-321.317 -440.815)" fill="#ff0513"/>
-            </svg>
+            <div className="icon">
+                <svg id="Group_66" data-name="Group 66" xmlns="http://www.w3.org/2000/svg" width="72.651" height="78.76" viewBox="0 0 72.651 78.76">
+                    <path id="Path_56" data-name="Path 56" d="M393.967,486.82,357.638,509.8,321.317,486.82l36.321-22.844Zm0,0" transform="translate(-321.317 -463.976)" fill="#ffe01a"/>
+                    <path id="Path_57" data-name="Path 57" d="M357.638,493.844,335.1,479.953l-13.783,8.436,36.321,22.384,36.329-22.384-13.781-8.436Zm0,0" transform="translate(-321.316 -448.543)" fill="#ff6f00"/>
+                    <path id="Path_58" data-name="Path 58" d="M357.639,502.646,335.1,488.753l-13.783,8.438,36.322,22.384,36.329-22.384-13.781-8.438Zm0,0" transform="translate(-321.317 -440.815)" fill="#ff0513"/>
+                </svg>
+            </div>
             <h4>{item.title}</h4>
             <p>{item.desc}</p>
         </div>
--- src/components/Testinomial.js
+++ src/components/Testinomial.js
@@ -5,7 +5,9 @@ function Testinomial( {data, breakPoint} ) {
 
     const listSlider = data.map(item => (
         <div className="slider-image">
-            <img id="test-image" src={item.image} alt="person" />
+            <div id="test-image">
+                <img src={item.image} alt="person" />
+            </div>  
             <div className="feedback">
                 <svg className="start-invert" xmlns="http://www.w3.org/2000/svg" width="76" height="208" viewBox="0 0 76 120">
                     <text id="_" data-name="&quot;" transform="translate(38 184)" fill="#ff0513" font-size="200" font-family="Haettenschweiler"><tspan x="-37.402" y="0">&quot;</tspan></text>
